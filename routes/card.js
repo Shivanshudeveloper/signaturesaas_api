e786b605036683a4248587f057d3480c169ccf6e
shivanshu_gupta1 <shivanshu981@gmail.com>
2021-09-04 10:32:40 +0530

    S

--- routes/card.js
+++ routes/card.js
@@ -7,4 +7,8 @@ const router = express.Router();
 router.post("/save", saveCard);
 router.get("/getsign", getCards);
 
+router.get("/test", (req, res) => {
+    res.send("Working");
+});
+
 export default router;
